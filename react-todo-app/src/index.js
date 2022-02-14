4b3ac9968d3519f125b675253768ae8709fa8bb4
asanchy <sanchyclips@gmail.com>
2022-02-14 15:36:19 +0200

    Setup linters and decomposed app UI into components

---
+++ react-todo-app/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoContainer from './components/TodoContainer';
+
+const element = <h1>Hello from Create React App</h1>;
+
+ReactDOM.render(<TodoContainer />, document.getElementById('root'));
